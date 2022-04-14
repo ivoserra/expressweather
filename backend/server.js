e8e4755f1591abe8cc32b54a075ee9deb0941098
Ivo Serra <dci.ivoserra@gmail.com>
2022-04-14 23:34:54 +0200

    dotenv

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from 'express'
 // import db from 'db'
 import axios from 'axios'
 import db from '../backend/db.js'
+import dotenv from 'dotenv'
 
 const app = express()
 
 app.use(express.json())
 
-
+dotenv.config()
+const { URL } = process.env
 
 app.get('/', (req, res)=>res.send('welcome to the weather api'))
 
@@ -16,7 +18,7 @@ app.post('/weather', (req, res)=>{
     // fetch the temperature of the requested city
     axios({
         method:"GET",
-        url:`https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=211da0ba8780f809b807739e63f4ba96&units=metric`,
+        url:URL,
     }).then(response => {
         const date = new Date().toISOString()
     
